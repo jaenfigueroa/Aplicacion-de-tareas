873a1a95fdd274ef60e75f14f37bba97500917f2
Jaen Figueroa <69079292+jaenfigueroa@users.noreply.github.com>
2022-10-15 14:55:42 -0500

    terminado, localstorage funcionando

--- main.js
+++ main.js
@@ -1,130 +1,104 @@
-/* Base de datos provisional -> contenedor de notas ////////*/
-let BaseDatosProvicional = [
-  // {
-  //   id: 1,
-  //   contenido: "nxsnxs xsjnxsnxs xsnxsxus xsnxsnxus xsjn 1"
-  // },
-]
+//BASE DE DATOS//////////////////////////////////////////
+let baseDatos = []
 
-/* formato para las nuevas notas ////////////////////////////*/
-class Nota {
-  constructor(id, contenido) {
-    this.id = id
-    this.contenido = contenido
-  }
+function guardarEnLocalStorage() {
+  localStorage.setItem('base-datos', JSON.stringify(baseDatos))
 }
-/* ///////////////////////////////////////////////// */
+
+function recuperValoresLocalStorage() {
+  return JSON.parse(localStorage.getItem('base-datos'))
+}
+
+//RECUPERAR LOS VALORES DEL LOCAL STORAGE Y RENDERIZARLOS POR PRIMERA VEZ
+window.addEventListener('load', () => {
+  baseDatos = recuperValoresLocalStorage()
+
+  MostrarTareas(baseDatos)
+})
+
+//RENDERIZAR LAS TAREAS////////
+function MostrarTareas() {
+
+  let contenido = ''
+
+  baseDatos.forEach(elemento => {
+    contenido = `
+    <article id='${elemento.id}' onclick="tacharTarea(${elemento.id})">
+    <p class='contenido ${elemento.tachado ? 'tachado' : ''}' >${elemento.contenido}</p>
+    <button class="boton-eliminar" onclick="eliminarTarea(${elemento.id})" >X</button>
+    </article>
+    ` + contenido
+  });
+
+  document.getElementById('contenedor-tareas').innerHTML = contenido
+
+  guardarEnLocalStorage(baseDatos)
+}
+
+
+//ALTERNAR EL EDITOR DE TEXTO///////////////
+const seccionEditar = document.querySelector('#seccion-escribir')
+const botonAgregar = document.querySelector('#boton-nueva-tarea')
+const botonSalir = document.querySelector('#boton-salir')
+
+botonAgregar.addEventListener('click', () => {
+  seccionEditar.style.display = 'flex'
+})
+
+botonSalir.addEventListener('click', () => {
+  seccionEditar.style.display = 'none'
+})
+
+//COMPROBAR CANTIDAD DE CARACTERES/////////
 const texto = document.querySelector('#texto')
+const botonGuardar = document.querySelector('#boton-guardar')
 const contador = document.querySelector('#contador')
 
-const botonGuardar = document.querySelector('#boton-guardar')
-
 texto.addEventListener('input', () => {
-  /* calcular numero de caracteres disponibles */
-  const maximo = 100
-  let usados = texto.value.length
+  let valor = 50 - texto.value.length
 
-  let disponibles = maximo - usados
-  /* imprimir numero de caracteres disponibles */
-  contador.textContent = disponibles
-
-  /* desactivar boton */
-  if (disponibles < 0) {
+  if (valor < 0) {
     botonGuardar.disabled = true
   } else {
     botonGuardar.disabled = false
   }
+
+  contador.textContent = valor
 })
 
-/* //////////////////////////////////////////////// */
-const contenedor = document.querySelector('#contenedor')
-
-botonGuardar.addEventListener('click', () => {
-
-  /* comprobar que el input no este vacio */
-  if (texto.value) {
-    /* crear los elemento necesarios*/
-    const articulo = document.createElement('article')
-    const p = document.createElement('p')
-    const boton = document.createElement('button')
-
-    /* crear id para la nota */
-    const id = Math.random() * (Math.pow(10, 16))
-
-    /* fusionar los elementos y contenido */
-    p.textContent = texto.value
-    boton.textContent = 'X'
-    articulo.append(p)
-    articulo.append(boton)
-
-    boton.setAttribute('id', id)
-    articulo.setAttribute('id', `contenedor-${id}`)
-
-    /* agregar la nueva nota nueva al contenedor de notas del html */
-    contenedor.prepend(articulo)
-
-    /* crear el objeto y guardar la informacion en la base de datos provicional */
-    const nuevaNota = new Nota(id, texto.value)
-
-    BaseDatosProvicional.push(nuevaNota)
-    console.log(BaseDatosProvicional);
-
-    /* agregarle un escuchador de eventos al boton de eliminar de la nueva tarea */
-    agregarEscuchadorEvento(id)
+//GUARDAR TAREAS/////////////////////////////
+class Tarea {
+  constructor(id, contenido, tachado) {
+    this.id = id
+    this.contenido = contenido
+    this.tachado = tachado
   }
-
-  /* eliminar el contendo del input despues de que el usuario lo guardo*/
-  texto.value = ''
-
-  // volver()
-})
-
-/* ELIMINAR NOTAS/TAREAS ////////////////////////////////////////// */
-
-/* agregar un escuchador de eventos a todos los botones de eliminar */
-const botonesDeBorrar = document.querySelectorAll('.boton-eliminar')
-
-for (const boton of botonesDeBorrar) {
-  agregarEscuchadorEvento(boton.id)
 }
 
-function agregarEscuchadorEvento(id) {
-  document.getElementById(`${id}`).addEventListener('click', () => {
-    /* para que elimine la tarea al que esta adjuntado*/
-    borrarTarea(id)
-  })
+botonGuardar.addEventListener('click', guardarTarea)
+
+function guardarTarea() {
+  const texto = document.querySelector('#texto').value
+  const id = Math.floor(Math.random() * 1000)
+
+  const nuevaTarea = new Tarea(id, texto, false)
+  baseDatos.push(nuevaTarea)
+
+  MostrarTareas()
 }
 
-function borrarTarea(idParaBorrar) {
-  /* verificar si existe la tarea en la base de datos provicional */
-  resultado = BaseDatosProvicional.some(x => x.id === idParaBorrar)
+//TACHAR TAREAS//////////////////////////////
+function tacharTarea(id) {
+  let tarea = baseDatos.filter(x => x.id == id)
+  tarea.some(x => x.tachado = !x.tachado)
 
-  if (resultado) {
-    console.log('existe la tarea');
-
-    /* eliminar la tarea de la base de datos provicional */
-    BaseDatosProvicional = BaseDatosProvicional.filter(x => x.id !== idParaBorrar)
-  }
-
-  /* eliminar al tarea del contenedor html */
-  document.getElementById(`contenedor-${idParaBorrar}`).remove()
+  MostrarTareas()
 }
 
-/* INTERCAMBIAR ENTRE SECCIONES//////////////////////////////////// */
+//ELIMINAR TAREAS////////////////////////////
+function eliminarTarea(idParaEliminar) {
+  baseDatos = baseDatos.filter(x => x.id !== idParaEliminar)
 
-/* mostrar la seccion de escribir/////////////////////////////////// */
-const seccionEscribir = document.getElementById('seccion-escribir')
-const seccionNotas = document.getElementById('seccion-tareas')
-
-document.querySelector('#boton-escribir').addEventListener('click', () => {
-  seccionEscribir.style.display = "flex"
-  // seccionNotas.style.display = 'none'
-})
-
-/* mostrar la seccion de notas/////////////////////////////////// */
-document.querySelector('#boton-salir').addEventListener('click', volver)
-
-function volver() {
-  seccionEscribir.style.display = "none"
-  // seccionNotas.style.display = 'flex'
+  MostrarTareas()
 }
+
